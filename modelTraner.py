b2ea1fdf626390643cf7e348c2a47288cfc5decd
Tom Davies <thomas.gilmour.davies@gmail.com>
2025-11-03 13:51:48 +0800

    Initial commit

---
+++ modelTraner.py
@@ -0,0 +1,31 @@
+"""
+File: modelTraner.py
+Description: This script is responsible for training a YOLO model with the purpose of detecting Red Bull cans in videos
+Data: 03-11-2025
+Author: Tom Davies
+"""
+
+
+###############################################
+# IMPORTS
+###############################################
+
+from ultralytics import YOLO
+import os
+
+###############################################
+# Training function
+###############################################
+def train_YOLO_model():
+    # Define data and pretrained model paths
+    
+    # Load model
+
+    # Train model
+
+    # Save model
+
+    # Perform evaluation
+
+if __name__ == "__main__":
+    train_YOLO_model()
