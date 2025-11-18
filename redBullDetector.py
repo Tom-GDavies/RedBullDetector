bd689b097946c7fb32e3c7732ec526716850a40b
Tom Davies <thomas.gilmour.davies@gmail.com>
2025-11-18 16:03:23 +0800

    Overall pipeline is working correctly

--- redBullDetector.py
+++ redBullDetector.py
@@ -15,47 +15,72 @@ import ultralytics
 import cv2
 import os
 
-
-###############################################
-# FUCNTION TO CREATE CLIPS GIVE START AND END FRAMES
-###############################################
-def create_clip(start_frame, end_frame, video_path, file_name, output_file_path):
-
-    # Create output path
-    clip_name = "clip_{}_{}_{}.mp4".format(file_name, start_frame, end_frame)
-    output_path = os.path.join(output_file_path, clip_name)
-
-    # Read frames from the original video
-    cap = cv2.VideoCapture(video_path)
-    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-    frames_to_write = end_frame - start_frame + 1
-
-    # Create video writer
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
-    # Write frames to new clip
-    while frames_to_write > 0:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        out.write(frame)
-        frames_to_write -= 1
-    
-    cap.release()
-    out.release()
-
 ###############################################
 # CONSTANTS
 ###############################################
 
-TARGET_FPS = 10  # Frames per second for processing
-MINIMUM_CONSECUTIVE = 5  # Minimum consecutive frames with detection to save
+TARGET_FPS = 3  # Frames per second for processing
+MINIMUM_CONSECUTIVE = 3  # Minimum consecutive frames with detection to save
 LEEWAY_FRAMES = 2 # Leeway given for missed Red Bull detections
 BORDER_FRAMES = 30 # Number of frames added before and after clip
+END = -1 # FOR TESTING ONLY | Maximum number of frames to process per video
+
+###############################################
+# FUCNTION TO CREATE CLIPS GIVE START AND END FRAMES
+###############################################
+def create_clip(key_frames, video_path, file_name, output_file_path):
+
+    # Print useful info
+    print(f"Creating clips for {file_name}...")
+
+    # Read frames from the original video
+    cap = cv2.VideoCapture(video_path)
+
+    frame_count = 0
+    clip_rolling = False
+
+    # Write frames to new clip
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if frame_count == END:  # FOR TESTING ONLY | Limit number of frames processed
+            break
+
+        frame_count += 1
+
+        # If current frame is either start of end
+        if frame_count in key_frames:
+
+            # Start of a new clip
+            if not clip_rolling:
+                start_frame = frame_count
+                clip_rolling = True
+
+                # Create get video properties
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                fps = int(cap.get(cv2.CAP_PROP_FPS))
+                width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+                # Create output path
+                clip_name = "clip_{}_{}.mp4".format(file_name, start_frame)
+                output_path = os.path.join(output_file_path, clip_name)
+
+                # Initialize video writer
+                out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+            # End of the current clip
+            else:
+                clip_rolling = False
+                out.release()
+
+        # If currently writing a clip, write the frame
+        if clip_rolling:
+            out.write(frame)
+                
+    cap.release()
 
 ###############################################
 # GET COMMAND LINE ARGUMENTS
@@ -103,12 +128,14 @@ for video_file in os.listdir(args.input_file_path):
     # Used to limit fps
     mod = max(1, round(fps / TARGET_FPS))
 
+    # Print useful info
+    print(f"Processing {video_file}: {width}x{height} at {fps} FPS (processing at ~{fps/mod} FPS)")
 
     frame_count = 0
     start_frame = 0
     consecutive = 0
     leeway = LEEWAY_FRAMES
-    clips = []
+    key_frames = set()
 
     ###############################################
     # LOOP THROUGH FRAMES
@@ -119,13 +146,16 @@ for video_file in os.listdir(args.input_file_path):
         if not ret:
             break # End of video
 
+        if frame_count == END:  # FOR TESTING ONLY | Limit number of frames processed
+            break
+
         frame_count += 1
 
         # Limit fps
         if frame_count % mod != 0:
             continue
 
-        results = model(frame) # Perform detection
+        results = model(frame, verbose=False) # Perform detection
 
         boxes = results[0].boxes
 
@@ -141,7 +171,9 @@ for video_file in os.listdir(args.input_file_path):
             if consecutive >= MINIMUM_CONSECUTIVE:
                 start = max(0, start_frame - BORDER_FRAMES)
                 end = min(frame_count + BORDER_FRAMES, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1)
-                create_clip(start, end, video_path, video_file.split('.')[0], args.output_file_path)
+                print(f"  - Detected Red Bull can from frame {start} to {end}")
+                key_frames.add(start)
+                key_frames.add(end)
 
             consecutive = 0
             leeway = LEEWAY_FRAMES
@@ -157,3 +189,6 @@ for video_file in os.listdir(args.input_file_path):
             consecutive += 1
 
     cap.release()
+
+
+    create_clip(key_frames, video_path, video_file.split('.')[0], args.output_file_path)
